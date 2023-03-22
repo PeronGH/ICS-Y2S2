08bd9cfa0b10484f52eb54a5530d22708fdda2a6
peron <peron_ap@icloud.com>
2023-03-22 15:09:22 +0800

    refactor: simplify isComplete

--- CPT102/BinaryTreeNode.ts
+++ CPT102/BinaryTreeNode.ts
@@ -25,43 +25,26 @@ export class BinaryTreeNode<T> {
     return this.size === 0;
   }
 
-  static isComplete<T>(root: BinaryTreeNode<T> | null): boolean {
-    // If the root is null, the tree is considered complete
-    if (!root) {
-      return true;
-    }
-
-    // Initialize a queue to traverse the tree using level order traversal
-    const queue: Array<BinaryTreeNode<T> | null> = [root];
-    // A flag to indicate whether a null node has been encountered or not
+  get isComplete(): boolean {
+    const queue: Array<BinaryTreeNode<T> | null> = [this];
     let flag = false;
 
-    // Loop until the queue is empty
     while (queue.length > 0) {
-      // Dequeue the first element from the queue
       const current = queue.shift()!;
 
-      // If the current node is null, set the flag to true
       if (current === null) {
         flag = true;
       } else {
-        // If a non-null node is found after a null node, the tree is not complete
         if (flag) {
           return false;
         }
 
-        // Enqueue the left and right children of the current node
         queue.push(current.left);
         queue.push(current.right);
       }
     }
 
-    // If the loop completes without returning false, the tree is complete
     return true;
-  }
-
-  get isComplete(): boolean {
-    return BinaryTreeNode.isComplete(this);
   }
 
   collect(order: "in" | "pre" | "post" = "in", result: T[] = []): T[] {
