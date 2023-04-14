0dd9c9c791c9b634cfa0064243c65c967f0cc6d8
peron <peron_ap@icloud.com>
2023-04-14 22:01:00 +0800

    refactor: rewrite simplex pipe with nix

--- CPT104/pipe/src/simplex.rs
+++ CPT104/pipe/src/simplex.rs
@@ -1,22 +1,26 @@
-use std::io::{Read, Write};
-use std::os::unix::net::UnixStream;
+use nix::unistd::{close, pipe, read, write};
+use std::ffi::CString;
 
-pub fn main() -> std::io::Result<()> {
+pub fn main() -> nix::Result<()> {
     // Step 1: Create a pipe
-    let (mut read_end, mut write_end) = UnixStream::pair()?;
+    let (read_fd, write_fd) = pipe()?;
 
     // Step 2: Write data into the pipe
-    let message = "Hello, Pipe!";
-    write_end.write_all(message.as_bytes())?;
+    let message = CString::new("Hello, Pipe!").unwrap();
+    write(write_fd, message.as_bytes_with_nul())?;
+
+    // Close the write file descriptor
+    close(write_fd)?;
 
     // Step 3: Read data from the pipe
     let mut buffer = [0; 128];
-    let bytes_read = read_end.read(&mut buffer)?;
-    let received_message = String::from_utf8_lossy(&buffer[..bytes_read]);
+    let bytes_read = read(read_fd, &mut buffer)?;
+    let received_message = unsafe { CString::from_vec_unchecked(buffer[..bytes_read].to_vec()) };
 
-    println!("Received message: {}", received_message);
+    println!("Received message: {:?}", received_message);
 
-    // No need for Step 4: Closing the pipe, as UnixStream will be closed automatically when they go out of scope
+    // Step 4: Close the pipe
+    close(read_fd)?;
 
     Ok(())
 }
