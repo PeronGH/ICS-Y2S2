3f8502fce428e0641d6fd55719847db799a8a2e0
peron <peron_ap@icloud.com>
2023-04-18 14:30:10 +0800

    clean: remove CPT104 exercises

--- CPT104/Exercises/ex-3-1.py
+++
@@ -1,5 +0,0 @@
-def main():
-    num_names = int(input())
-    for _ in range(num_names):
-        [last_name, first_name] = input().split()
-        print(first_name, last_name)
--- CPT104/Exercises/ex1-2.py
+++
@@ -1,3 +0,0 @@
-n = input()
-for i in range(0, 11):
-    print(f'{n}x{i} = {n*i}')
