a1b52c90f60092547761417c590ee140eee1f569
peron <peron_ap@icloud.com>
2023-05-09 18:18:54 +0800

    feat: calculate bound

--- INT102/week9/src/lib.rs
+++ INT102/week9/src/lib.rs
@@ -1,6 +1,5 @@
-use std::fmt::Debug;
-
 use petgraph::graph::{NodeIndex, UnGraph};
+use std::fmt::Debug;
 
 pub fn travelling_salesman<N: Clone + Debug>(
     graph: &UnGraph<N, f64>,
@@ -23,6 +22,25 @@ pub fn travelling_salesman<N: Clone + Debug>(
     (best_tour, best_cost)
 }
 
+fn lower_bound<N: Clone + Debug>(graph: &UnGraph<N, f64>, path: &Vec<NodeIndex>) -> f64 {
+    let mut lb = 0.0;
+    for node in graph.node_indices() {
+        if !path.contains(&node) {
+            let mut min_edges = graph
+                .edges(node)
+                .map(|edge| *edge.weight())
+                .collect::<Vec<f64>>();
+            min_edges.sort_by(|a, b| a.partial_cmp(b).unwrap());
+            if min_edges.len() >= 2 {
+                lb += (min_edges[0] + min_edges[1]) / 2.0;
+            } else if min_edges.len() == 1 {
+                lb += min_edges[0];
+            }
+        }
+    }
+    lb
+}
+
 fn tsp_helper<N: Clone + Debug>(
     graph: &UnGraph<N, f64>,
     current: NodeIndex,
@@ -32,21 +50,9 @@ fn tsp_helper<N: Clone + Debug>(
     best_cost: &mut f64,
 ) {
     // Base case: If the current path has visited all the cities
-    // (i.e., the length of the path is equal to the number of cities in the graph),
-    // the algorithm checks if the cost of the current tour (including the cost of returning to the starting city)
-    // is less than the best cost found so far. If it is, the best tour and best cost are updated accordingly.
     if path.len() == graph.node_count() {
         let last_edge = graph.find_edge(current, path[0]).unwrap();
         let total_cost = cost + graph[last_edge];
-
-        println!(
-            "\nPath: {:?}",
-            path.iter()
-                .chain([path[0]].iter())
-                .map(|i| graph.node_weight(*i).unwrap())
-                .collect::<Vec<_>>()
-        );
-        println!("Cost: {:?}", total_cost);
 
         if total_cost < *best_cost {
             *best_cost = total_cost;
@@ -55,27 +61,27 @@ fn tsp_helper<N: Clone + Debug>(
         return;
     }
 
-    // Recursive step: For each neighbor of the current city that has not been visited yet (i.e., not in the current path),
-    // the algorithm calculates the cost of moving to that neighbor. If the new cost is less than the current best cost,
-    // it proceeds with the recursion by calling the tsp function again with the neighbor as the new current city,
-    // an updated path that includes the neighbor, and the new cost.
+    // Recursive step: For each neighbor of the current city that has not been visited yet
     for neighbor in graph.neighbors(current) {
         if !path.contains(&neighbor) {
             let edge = graph.find_edge(current, neighbor).unwrap();
             let new_cost = cost + graph[edge];
-            if new_cost < *best_cost {
-                let mut new_path = path.clone();
-                new_path.push(neighbor);
+            let mut new_path = path.clone();
+            new_path.push(neighbor);
 
-                println!(
-                    "\nPath: {:?}",
-                    new_path
-                        .iter()
-                        .map(|i| graph.node_weight(*i).unwrap())
-                        .collect::<Vec<_>>()
-                );
-                println!("Cost: {:?}", new_cost);
+            let bound = new_cost + lower_bound(graph, &new_path);
 
+            println!(
+                "Path: {:?}\nCost: {}\nBound: {}\n",
+                new_path
+                    .iter()
+                    .map(|n| graph.node_weight(*n).unwrap())
+                    .collect::<Vec<_>>(),
+                new_cost,
+                bound
+            );
+
+            if bound < *best_cost {
                 tsp_helper(graph, neighbor, new_path, new_cost, best_tour, best_cost);
             }
         }
